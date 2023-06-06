3e09f7f00eefcceace49f8c8d350a62efbe24980
phamphuong140323 <phuongptph25249@fpt.edu.vn>
2023-06-06 19:54:06 +0700

    feat:AN-59 home-page ref: AN-59

--- src/app/components/client/product-page/product-page.component.ts
+++ src/app/components/client/product-page/product-page.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { ProductService } from 'src/app/services/product.service';
+import { IProduct } from 'src/app/interfaces/product';
 @Component({
   selector: 'app-product-page',
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
 export class ProductPageComponent {
-
+  products : IProduct[] = []
+  constructor(private ProductService:ProductService){
+    this.ProductService.getAllProduct().subscribe(data =>{
+      this.products = data
+    })
+  }
 }
