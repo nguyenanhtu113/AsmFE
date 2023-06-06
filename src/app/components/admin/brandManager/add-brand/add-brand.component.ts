563eb547a8e8d581b69a18bf9d1ffea99c02a532
duongduclong2308 <longddph26364@fpt.edu.vn>
2023-06-07 00:14:35 +0700

    feat: Add+get list brand ref: AN-83

--- src/app/components/admin/brandManager/add-brand/add-brand.component.ts
+++ src/app/components/admin/brandManager/add-brand/add-brand.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms"
+import { Router } from '@angular/router';
+import { IBrand } from 'src/app/interfaces/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-add-brand',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-brand.component.css']
 })
 export class AddBrandComponent {
+  constructor(private formBuilder: FormBuilder,
+    private brandService: BrandService,
+    private back: Router
+  ) { }
 
+  brandForm = this.formBuilder.group({
+    name: ['']
+  })
+
+  onHandleSubmit() {
+    if (this.brandForm.valid) {
+      const brand: IBrand = {
+        name: this.brandForm.value.name || "",
+      }
+      this.brandService.addBrand(brand).subscribe((data) => {
+        this.back.navigate(['admin', 'brand'])
+      })
+    }
+  }
 }
