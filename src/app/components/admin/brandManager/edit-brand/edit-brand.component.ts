99e1332b9e74074820cef229323509ad5a62cfcb
duongduclong2308 <longddph26364@fpt.edu.vn>
2023-06-07 13:56:47 +0700

    feat: edit brand + get1Brand ref: AN-84

--- src/app/components/admin/brandManager/edit-brand/edit-brand.component.ts
+++ src/app/components/admin/brandManager/edit-brand/edit-brand.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { IBrand } from 'src/app/interfaces/brand';
+import { BrandService } from 'src/app/services/brand.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-brand',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-brand.component.css']
 })
 export class EditBrandComponent {
+  brand!: IBrand
+  brandForm = this.formBuilder.group({
+    name: ['']
+  })
+  constructor(
+    private brandService: BrandService,
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private back: Router
+  ) {
+    this.route.paramMap.subscribe(param => {
+      const id = param.get('id');
+      this.brandService.getOneBrand(id).subscribe(brand => {
+        this.brand = brand;
+        this.brandForm.patchValue({
+          name: brand.name,
+        })
+      })
+    })
+  }
 
+  onHandleEdit() {
+    if (this.brandForm.valid) {
+      const product: IBrand = {
+        id: this.brand.id,
+        name: this.brandForm.value.name || "",
+      }
+      this.brandService.editBrand(product).subscribe((data) => {
+        this.back.navigate(['admin', 'brand'])
+      })
+    }
+  }
 }
