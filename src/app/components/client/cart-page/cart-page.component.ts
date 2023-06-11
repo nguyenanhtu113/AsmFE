10c53dc43e699342b45f210d2bf6fcf74d2d71f7
xnavd <duongxuanvan.official@gmail.com>
2023-06-12 01:12:36 +0700

    add to cart

--- src/app/components/client/cart-page/cart-page.component.ts
+++ src/app/components/client/cart-page/cart-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart-page',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-page.component.css']
 })
 export class CartPageComponent {
+  cartItems: any[] = [];
 
+  constructor(private cartService: CartService) {}
+
+
+  removeFromCart(item: any) {
+    // Thực hiện các thao tác để xóa sản phẩm khỏi giỏ hàng
+    // Ví dụ: sử dụng service để xóa sản phẩm khỏi danh sách
+    this.cartService.removeFromCart(item);
+  }
+
+  ngOnInit() {
+    this.cartService.restoreCartItems();
+    this.cartItems = this.cartService.cartItems;
+  }
 }
